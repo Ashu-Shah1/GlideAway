2a6be2ab4abb4cd2664950ebd9c000106068ea29
NegiGauravGit <gauravsinghnegi54@gmail.com>
2025-03-06 21:32:36 +0530

    modified app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import TravelCommunity from "./components/TravelCommunity";
 import Header from './components/Header';
@@ -13,29 +13,46 @@ import TransportOptions from './components/TransportOptions';
 import Activities from './components/Activities'; 
 import InteractiveMap from './components/InteractiveMap';
 
+const Layout = ({ children }) => {
+  const location = useLocation();
+  
+  // Hide header and navbar on /auth page
+  const hideHeaderAndNavbar = location.pathname === "/auth";
+
+  return (
+    <div>
+      {!hideHeaderAndNavbar && <Navbar />}
+      {!hideHeaderAndNavbar && <Header />}
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        {children}
+      </main>
+      {!hideHeaderAndNavbar && <Footer />}
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <Router>
-      <div>
-        <Navbar />
-        <Header />
-        <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-          <Routes>
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/" element={<>
-              <DestinationSearch />
-              <WeatherUpdate />
-              <InteractiveMap />
-              <TransportOptions />
-              <PopularDestinations />
-              <Activities />
-              <TravelCommunity />
-            </>} />
-            <Route path="/community-post" element={<CommunityPostPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/" element={
+          <Layout>
+            <DestinationSearch />
+            <WeatherUpdate />
+            <InteractiveMap />
+            <TransportOptions />
+            <PopularDestinations />
+            <Activities />
+            <TravelCommunity />
+          </Layout>
+        } />
+        <Route path="/community-post" element={
+          <Layout>
+            <CommunityPostPage />
+          </Layout>
+        } />
+      </Routes>
     </Router>
   );
 };
