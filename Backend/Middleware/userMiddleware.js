f42c9a486711a545ea04b40169ff0607d870a513
NegiGauravGit <gauravsinghnegi54@gmail.com>
2025-03-12 21:00:10 +0530

    added middleware for user

---
+++ Backend/Middleware/userMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken'
+
+function userAuth(req,res,next){
+    const token = req.cookies?.token
+
+    if(!token){
+        return res.status(401).json({ msg: "No token provided. Unauthorized" });
+    }
+    jwt.verify(token,process.env.JWT_USER_SECRET,function(err,decoded){
+        if(err){
+            return res.status(403).json({ msg: "Invalid or expired token. Access denied" });
+        }
+        req.userId = decoded.id
+        next()
+    })
+}
+module.exports = {
+    userAuth:userAuth
+}
