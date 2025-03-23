58450c4e7d47edc982d04c885064d2087f438288
NegiGauravGit <gauravsinghnegi54@gmail.com>
2025-03-23 23:15:37 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import mongoose  from 'mongoose';
 import dotenv from 'dotenv';
-dotenv.config({path:'frontend/Backend/.env'});
+dotenv.config();
 import userRouter from './Routes/user.js'
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
 
+console.log(process.env.JWT_USER_SECRET)
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
@@ -16,10 +17,10 @@ app.use("/destination",userRouter)
 
 async function main(){
     try {
-        await mongoose.connect("mongodb+srv://gauravsinghnegi54:DONjii@cluster0.notds.mongodb.net/GlideAway-Web");
+        await mongoose.connect(process.env.DB_CONNECTION_STRING);
         console.log("Connected to MongoDB");
     
-        const PORT = 3000;
+        const PORT = process.env.PORT;
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
       } catch (err) {
         console.error("MongoDB connection error:", err);
