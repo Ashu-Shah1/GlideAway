24a02fee50a842562ba6a0f6307c7bbe4f30ec9c
NegiGauravGit <gauravsinghnegi54@gmail.com>
2025-04-29 22:08:44 +0530

    updated

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,20 +1,11 @@
-// Auth.jsx
+// components/Authentication.js
 import React from 'react';
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { SignIn } from "@clerk/clerk-react";
 
 const Auth = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[70vh] space-y-6">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <button className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
-            Sign In
-          </button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton afterSignOutUrl="/" />
-      </SignedIn>
+    <div className="flex justify-center items-center min-h-screen">
+      <SignIn path="/auth" routing="path" signUpUrl="/auth" />
     </div>
   );
 };
